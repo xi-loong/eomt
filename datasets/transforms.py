75e6aab765d90aacdc937e0735a868ac3075fe57
Tommie Kerssies <tommiekerssies@gmail.com>
2025-05-03 14:10:01 +0200

    Fix critical bug in transforms.py breaking training

--- datasets/transforms.py
+++ datasets/transforms.py
@@ -6,11 +6,12 @@
 # used under the Apache 2.0 License.
 # ---------------------------------------------------------------
 
-
 import torch
 from torchvision.transforms import v2 as T
 from torchvision.transforms.v2 import functional as F
-from torch import nn
+from torchvision.tv_tensors import wrap, TVTensor
+from torch import nn, Tensor
+from typing import Any, Union
 
 
 class Transforms(nn.Module):
@@ -19,10 +20,10 @@ class Transforms(nn.Module):
         img_size: tuple[int, int],
         color_jitter_enabled: bool,
         scale_range: tuple[float, float],
-        max_brightness_delta=32,
-        max_contrast_factor=0.5,
-        saturation_factor=0.5,
-        max_hue_delta=18,
+        max_brightness_delta: int = 32,
+        max_contrast_factor: float = 0.5,
+        saturation_factor: float = 0.5,
+        max_hue_delta: int = 18,
     ):
         super().__init__()
 
@@ -34,62 +35,55 @@ class Transforms(nn.Module):
         self.max_hue_delta = max_hue_delta / 360.0
 
         self.random_horizontal_flip = T.RandomHorizontalFlip()
-
-        self.scale_jitter = T.ScaleJitter(
-            target_size=img_size,
-            scale_range=scale_range,
-        )
-
+        self.scale_jitter = T.ScaleJitter(target_size=img_size, scale_range=scale_range)
         self.random_crop = T.RandomCrop(img_size)
 
-    def random_factor(self, factor, center=1.0):
+    def _random_factor(self, factor: float, center: float = 1.0):
         return torch.empty(1).uniform_(center - factor, center + factor).item()
 
-    def brightness(self, img):
+    def _brightness(self, img):
         if torch.rand(()) < 0.5:
             img = F.adjust_brightness(
-                img, brightness_factor=self.random_factor(self.max_brightness_factor)
+                img, self._random_factor(self.max_brightness_factor)
             )
 
         return img
 
-    def contrast(self, img):
+    def _contrast(self, img):
         if torch.rand(()) < 0.5:
-            img = F.adjust_contrast(
-                img,
-                contrast_factor=self.random_factor(self.max_contrast_factor),
-            )
+            img = F.adjust_contrast(img, self._random_factor(self.max_contrast_factor))
 
         return img
 
-    def saturation_and_hue(self, img):
+    def _saturation_and_hue(self, img):
         if torch.rand(()) < 0.5:
             img = F.adjust_saturation(
-                img,
-                saturation_factor=self.random_factor(self.max_saturation_factor),
+                img, self._random_factor(self.max_saturation_factor)
             )
 
         if torch.rand(()) < 0.5:
-            img = F.adjust_hue(
-                img,
-                hue_factor=self.random_factor(self.max_hue_delta, center=0.0),
-            )
+            img = F.adjust_hue(img, self._random_factor(self.max_hue_delta, center=0.0))
 
         return img
 
     def color_jitter(self, img):
-        img = self.brightness(img)
+        if not self.color_jitter_enabled:
+            return img
+
+        img = self._brightness(img)
 
         if torch.rand(()) < 0.5:
-            img = self.contrast(img)
-            img = self.saturation_and_hue(img)
+            img = self._contrast(img)
+            img = self._saturation_and_hue(img)
         else:
-            img = self.saturation_and_hue(img)
-            img = self.contrast(img)
+            img = self._saturation_and_hue(img)
+            img = self._contrast(img)
 
         return img
 
-    def pad(self, img, target: dict):
+    def pad(
+        self, img: Tensor, target: dict[str, Any]
+    ) -> tuple[Tensor, dict[str, Union[Tensor, TVTensor]]]:
         pad_h = max(0, self.img_size[-2] - img.shape[-2])
         pad_w = max(0, self.img_size[-1] - img.shape[-1])
         padding = [0, 0, pad_w, pad_h]
@@ -99,31 +93,26 @@ class Transforms(nn.Module):
 
         return img, target
 
-    def forward(self, img, target: dict):
-        img_ = img.clone()
-        target_ = {
-            "masks": target["masks"][~target["is_crowd"]].clone(),
-            "labels": target["labels"][~target["is_crowd"]].clone(),
-        }
+    def _filter(self, target: dict[str, Union[Tensor, TVTensor]], keep: Tensor) -> dict:
+        return {k: wrap(v[keep], like=v) for k, v in target.items()}
 
-        if self.color_jitter_enabled:
-            img_ = self.color_jitter(img_)
+    def forward(
+        self, img: Tensor, target: dict[str, Union[Tensor, TVTensor]]
+    ) -> tuple[Tensor, dict[str, Union[Tensor, TVTensor]]]:
+        img_orig, target_orig = img, target
 
-        img_, target_ = self.random_horizontal_flip(img_, target_)
+        target = self._filter(target, ~target["is_crowd"])
 
-        img_, target_ = self.scale_jitter(img_, target_)
+        img = self.color_jitter(img)
+        img, target = self.random_horizontal_flip(img, target)
+        img, target = self.scale_jitter(img, target)
+        img, target = self.pad(img, target)
+        img, target = self.random_crop(img, target)
 
-        img_, target_ = self.pad(img_, target_)
+        valid = target["masks"].flatten(1).any(1)
+        if not valid.any():
+            return self(img_orig, target_orig)
 
-        img_, target_ = self.random_crop(img_, target_)
+        target = self._filter(target, valid)
 
-        mask_sums = target_["masks"].sum(dim=[-2, -1])
-        non_empty_mask = mask_sums > 0
-
-        if non_empty_mask.sum() == 0:
-            return self(img, target)
-
-        target_["masks"] = target_["masks"][non_empty_mask]
-        target_["labels"] = target_["labels"][non_empty_mask]
-
-        return img_, target_
+        return img, target
